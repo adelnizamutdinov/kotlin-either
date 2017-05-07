2b97b1ae9b884739379d0a4d354cd2a6a0f82058
adelnizamutdinov <stiggpwnz@gmail.com>
2017-05-07 14:25:05 +0300

    bump kotlin, bring back fold

--- src/test/kotlin/either/EitherTest.kt
+++ src/test/kotlin/either/EitherTest.kt
@@ -4,36 +4,36 @@ import org.junit.Test
 
 class EitherTest {
 
-  @Test
-  fun left() {
-    val x: Either<Int, Int> = Left(123)
+    @Test
+    fun left() {
+        val x: Either<Int, Int> = Left(123)
 
-    val la = when (x) {
-      is Left  -> x.value * 2
-      is Right -> -1
+        val la = when (x) {
+            is Left  -> x.value * 2
+            is Right -> -1
+        }
+
+        check(la == 123 * 2)
     }
 
-    check(la == 123 * 2)
-  }
+    @Test
+    fun right() {
+        val y: Either<Int, String> = Right("right")
 
-  @Test
-  fun right() {
-    val y: Either<Int, String> = Right("right")
+        val lala = when (y) {
+            is Left  -> "${y.value}left"
+            is Right -> "${y.value}right"
+        }
 
-    val lala = when (y) {
-      is Left  -> "${y.value}left"
-      is Right -> "${y.value}right"
+        check(lala == "rightright")
     }
 
-    check(lala == "rightright")
-  }
+    @Test
+    fun map() {
+        val l: Either<Int, String> = Left(5)
 
-  @Test
-  fun map() {
-    val l: Either<Int, String> = Left(5)
+        check(l.map { it + "yes" } == Left(5))
 
-    check(l.map { it + "yes" } == Left(5))
-
-    check(Right(5).map { it * 5 } == Right(25))
-  }
+        check(Right(5).map { it * 5 } == Right(25))
+    }
 }
